c7f0177198358e180fb5a6fb0e7dc3e01fc71d2d
Aplet123 <aplet@aplet.me>
2021-06-20 10:26:06 -0400

    bug fix+documentation

--- README.md
+++ README.md
@@ -1,3 +1,59 @@
 # kctf-pow
 
 A library and CLI to solve, check, and generate proof-of-work challenges using [kCTF](https://google.github.io/kctf/)'s scheme.
+
+# Installation
+
+For use as a library, add the [`kctf-pow`](https://crates.io/crates/kctf-pow) crate into your dependencies.
+
+The CLI can be installed with `cargo install kctf-pow`, or by cloning the repository, building with `cargo build --release`, and manually copying the executable.
+
+# CLI Usage
+
+To solve a challenge and print the solution to stdout:
+```
+kctf-pow solve <challenge>
+```
+For example:
+```bash
+kctf-pow solve s.AAAAMg==.NDtqORW1uZlIgzszbdMGZA==
+# Outputs s.NUH3arymnKB+ysUGdv+67ypDamn4wOKCPORB2ivWE1Yhinam2v4S6q4nAoC5LP97LScdVoq+NuFVF++Win5mNRYZS6bJAs8fk0h8XgvfcC/7JfmFISqeCIo/CIUgIucVAM+eGDjqitRULGXqIOyviJoJjW8DMouMRuJM/3eg/z18kutQHkX0N3sqPeF7Nzkk8S3Bs6aiHUORM30syUKYug==
+```
+To check a solution for a challenge:
+```
+kctf-pow check <challenge>
+```
+The solution is read from stdin. If the solution is correct, the program will exit with status code 0 and `correct` will be outputted. If the solution is incorrect, the program will exit with status code 1 and `incorrect` will be outputted. If the solution is malformed, the program will exit with status code 1 and an error message will be printed to stderr.
+
+For example:
+```bash
+kctf-pow check s.AAAAMg==.NDtqORW1uZlIgzszbdMGZA==
+# Input s.NUH3arymnKB+ysUGdv+67ypDamn4wOKCPORB2ivWE1Yhinam2v4S6q4nAoC5LP97LScdVoq+NuFVF++Win5mNRYZS6bJAs8fk0h8XgvfcC/7JfmFISqeCIo/CIUgIucVAM+eGDjqitRULGXqIOyviJoJjW8DMouMRuJM/3eg/z18kutQHkX0N3sqPeF7Nzkk8S3Bs6aiHUORM30syUKYug==
+# Outputs correct and exits with status code 0
+```
+
+To randomly generate a challenge:
+```
+kctf-pow gen <difficulty>
+```
+For example:
+```bash
+kctf-pow gen 50
+# Outputs s.AAAAMg==.NDtqORW1uZlIgzszbdMGZA==
+```
+
+To chain challenge generation and checking:
+```
+kctf-pow ask <difficulty>
+```
+For example:
+```bash
+kctf-pow ask 50
+# Outputs s.AAAAMg==.NDtqORW1uZlIgzszbdMGZA==
+# Input s.NUH3arymnKB+ysUGdv+67ypDamn4wOKCPORB2ivWE1Yhinam2v4S6q4nAoC5LP97LScdVoq+NuFVF++Win5mNRYZS6bJAs8fk0h8XgvfcC/7JfmFISqeCIo/CIUgIucVAM+eGDjqitRULGXqIOyviJoJjW8DMouMRuJM/3eg/z18kutQHkX0N3sqPeF7Nzkk8S3Bs6aiHUORM30syUKYug==
+# Outputs correct and exits with status code 0
+```
+
+# Library Documentation
+
+The documentation for the library is available on [docs.rs](https://docs.rs/kctf-pow).
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,44 @@
+//! A library to solve, check, and generate proof-of-work challenges using [kCTF](https://google.github.io/kctf/)'s scheme.
+//! ```rust
+//! use kctf_pow::KctfPow;
+//! 
+//! fn main() {
+//!     let pow = KctfPow::new();
+//!     // decoding then solving a challenge
+//!     let chall = pow.decode_challenge("s.AAAAMg==.H+fPiuL32DPbfN97cpd0nA==").unwrap();
+//!     println!("{}", chall.solve());
+//!     // decoding then checking a challenge
+//!     let chall = pow.decode_challenge("s.AAAAMg==.NDtqORW1uZlIgzszbdMGZA==").unwrap();
+//!     let sol = "s.NUH3arymnKB+ysUGdv+67ypDamn4wOKCPORB2ivWE1Yhinam2v4S6q4nAoC5LP97LScdVoq+NuFVF++Win5mNRYZS6bJAs8fk0h8XgvfcC/7JfmFISqeCIo/CIUgIucVAM+eGDjqitRULGXqIOyviJoJjW8DMouMRuJM/3eg/z18kutQHkX0N3sqPeF7Nzkk8S3Bs6aiHUORM30syUKYug==";
+//!     assert_eq!(chall.check(sol), Ok(true));
+//!     assert_eq!(chall.check("s.asdf"), Ok(false));
+//!     // generating a random challenge of difficulty 50
+//!     let chall = pow.generate_challenge(50);
+//!     println!("{}", chall);
+//! }
+//! ```
+
 use rand::prelude::*;
 use rug::integer::Order;
 use rug::ops::Pow;
 use rug::Integer;
+use std::convert::TryInto;
 use std::fmt;
 
 const VERSION: &'static str = "s";
 
+/// A proof-of-work system for kCTF.
+///
+/// All proof-of-work related methods are on instances of [`KctfPow`] in order to initialize and reuse related constants.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct KctfPow {
     modulus: Integer,
     exponent: Integer,
 }
 
+/// A proof-of-work challenge.
+///
+/// Contains a reference to the [`KctfPow`] that created the challenge. If you want to serialize it to a string, use the [`Display`](std::fmt::Display) implementation.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Challenge<'a> {
     difficulty: u32,
@@ -20,12 +47,17 @@ pub struct Challenge<'a> {
 }
 
 impl KctfPow {
+    /// Create a new instance and initialize necessary constants.
     pub fn new() -> Self {
         let modulus: Integer = Integer::from(2).pow(1279) - 1;
         let exponent = (modulus.clone() + 1) / 4;
         Self { modulus, exponent }
     }
 
+    /// Decodes a challenge from a string and returns it.
+    ///
+    /// For optimization purposes, the difficulty of the challenge must be able to fit in a [`u32`].
+    /// This shouldn't be an issue, since difficulties that can't fit into a [`u32`] will probably take too long anyways.
     pub fn decode_challenge(&self, chall_string: &str) -> Result<Challenge, &'static str> {
         let mut parts = chall_string.split('.');
         if parts.next() != Some(VERSION) {
@@ -40,18 +72,27 @@ impl KctfPow {
             .map(|x| base64::decode(x).map_err(|_| "Parts aren't valid base64"))
             .collect::<Result<_, _>>()?;
         let difficulty_bytes = &decoded_data[0];
+        let difficulty: u32;
         if difficulty_bytes.len() > 4 {
-            return Err("Difficulty is too large");
+            let (first, last) = difficulty_bytes.split_at(difficulty_bytes.len() - 4);
+            // if difficulty is 0-padded to longer than 4 bytes it should still work
+            if first.iter().any(|&x| x != 0) {
+                return Err("Difficulty is too large");
+            }
+            difficulty = u32::from_be_bytes(last.try_into().unwrap())
+        } else {
+            let mut difficulty_array = [0; 4];
+            difficulty_array[4 - difficulty_bytes.len()..].copy_from_slice(difficulty_bytes);
+            difficulty = u32::from_be_bytes(difficulty_array);
         }
-        let mut difficulty_array = [0; 4];
-        difficulty_array[4 - difficulty_bytes.len()..].copy_from_slice(difficulty_bytes);
         Ok(Challenge {
             pow: self,
             val: Integer::from_digits(&decoded_data[1], Order::Msf),
-            difficulty: u32::from_be_bytes(difficulty_array),
+            difficulty,
         })
     }
 
+    /// Generates a random challenge given a difficulty.
     pub fn generate_challenge(&self, difficulty: u32) -> Challenge {
         let mut bytes: [u8; 16] = [0; 16];
         thread_rng().fill(&mut bytes[..]);
@@ -70,6 +111,7 @@ impl Default for KctfPow {
 }
 
 impl<'a> Challenge<'a> {
+    /// Solves a challenge and returns the solution.
     pub fn solve(mut self) -> String {
         for _ in 0..self.difficulty {
             // guaranteed to succeed so ignore the result
@@ -83,6 +125,7 @@ impl<'a> Challenge<'a> {
         )
     }
 
+    /// Checks a solution to see if it satisfies the challenge.
     pub fn check(&self, sol: &str) -> Result<bool, &'static str> {
         let mut parts = sol.split('.');
         if parts.next() != Some(VERSION) {
@@ -90,7 +133,7 @@ impl<'a> Challenge<'a> {
         }
         let data = match parts.next() {
             Some(x) => x,
-            None => return Err("Incorrect number of parts")
+            None => return Err("Incorrect number of parts"),
         };
         if let Some(_) = parts.next() {
             return Err("Incorrect number of parts");
